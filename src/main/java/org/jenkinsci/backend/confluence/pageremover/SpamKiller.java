7ca085bac47ec16405b848d77d5b574f96aa04ff
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-03 22:30:03 -0800

    switched to the current attack mode

--- src/main/java/org/jenkinsci/backend/confluence/pageremover/SpamKiller.java
+++ src/main/java/org/jenkinsci/backend/confluence/pageremover/SpamKiller.java
@@ -94,8 +94,8 @@ public class SpamKiller extends JFrame {
     private void scan() {
         runAsync(new Callable<Void>() {
             public Void call() throws Exception {
-                possibleSpams = app.spellCheck();
-//                possibleSpams = app.watchFreeVideo();
+//                possibleSpams = app.spellCheck();
+                possibleSpams = app.watchFreeVideo();
 //                possibleSpams = fakeData();
 
                 spamFlag.clear();
