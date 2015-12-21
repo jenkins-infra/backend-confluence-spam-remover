de776da60fc9989f2a529ba5560a3d84054aeb90
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-12-21 08:36:11 -0800

    lower-case canonicalization

--- src/main/java/org/jenkinsci/backend/confluence/pageremover/WordList.java
+++ src/main/java/org/jenkinsci/backend/confluence/pageremover/WordList.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
-import java.util.StringTokenizer;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -41,6 +41,7 @@ public class WordList {
      * Checks if the word appears in the given text. This is without any fancy tokenizations.
      */
     public boolean matches(String longText) {
+        longText = longText.toLowerCase(Locale.ENGLISH);
         for (String word : words) {
             if (longText.contains(word))
                 return true;
